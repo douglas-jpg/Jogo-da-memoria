618666619bff5150b8cf7594f9450f39bd38ad86
Douglas Lima <57305990+douglas-jpg@users.noreply.github.com>
2023-06-04 14:07:48 -0300

    interface ranking
    adicionado a interface do ranking e a funcionalidade dos botões de navegar entre a pagina inicial e o raking

--- js/login.js
+++ js/login.js
@@ -1,6 +1,7 @@
-const login = document.querySelector('.login')
-const botao = document.querySelector('.botao_jogar')
-const nome = document.querySelector('.nome_login')
+const login = document.querySelector('.login');
+const botao = document.querySelector('.botao_jogar');
+const nome = document.querySelector('.nome_login');
+const voltar = document.querySelector('.voltar')
 
 function validarNome ({ target }) {
     if (target.value.length > 3) {
@@ -8,13 +9,17 @@ function validarNome ({ target }) {
         return;
     }
     botao.setAttribute('disabled', '');
-}
+};
 
 function guardarNome(event){
     event.preventDefault();
     localStorage.setItem('jogador', nome.value);
     window.location = 'pages/game.html'
-}
+};
+
+voltar.onclick = function b_voltar(){
+    window.location = 'pages/ranking.html'
+};
 
 login.addEventListener('input', validarNome);
 login.addEventListener('submit', guardarNome);
---
+++ js/ranking.js
@@ -0,0 +1,5 @@
+const menu = document.querySelector('.menu');
+
+menu.onclick = function b_menu(){
+    window.location = '../index.html'
+};
