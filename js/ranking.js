1094fb5c8e289cca468bc67a011c8bc9177524bc
Douglas Lima <57305990+douglas-jpg@users.noreply.github.com>
2023-06-06 21:51:09 -0300

    Atualizar o ranking.js
    início da função para organizar o ranking

--- js/ranking.js
+++ js/ranking.js
@@ -3,3 +3,12 @@ const menu = document.querySelector('.menu');
 menu.onclick = function b_menu(){
     window.location = '../index.html'
 };
+
+// fazer função para receber o nome e tempo do player
+
+function player(nome, tempo) {
+    return{
+        nome,
+        tempo
+    };
+}
